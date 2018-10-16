d3ffaff84f7cb068374cf5c3fd42df853989137b
alex.villecco <alex.villecco@globant.com>
2018-10-16 17:47:34 -0300

    Update ProjectCard design

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import '../App.css';
 
-import { Grid, Row, Col, } from 'react-bootstrap';
+import { Grid, Row, Col, Button, } from 'react-bootstrap';
 
 export default class ProjectCard extends Component {
     constructor(props) {
@@ -13,17 +13,25 @@ export default class ProjectCard extends Component {
             <div class="projectCard">
                 <Grid>
                     <Row>
-                        <Col xs={12} md={8}>
-                            <Row className="gridContainer">
+                        <Col xs={8} md={6}>
+                            <Row className="cellContainer">
                                 {this.props.projectName}
                             </Row>
-                            <Row className="gridContainer">
+                            <Row className="cellContainer">
                                 {this.props.projectAuthor}
                             </Row>
                         </Col>
-                        <Col xs={6} md={4}>
-                            <Row className="gridContainer">
+                        <Col xs={5} md={3}>
+                            <Row className="cellContainer">
+                                Total de inversiones recibidas
+                            </Row>
+                            <Row className="cellContainer">
                                 $ {this.props.totalInvestment}
+                            </Row>
+                        </Col>
+                        <Col xs={5} md={3}>
+                            <Row className="cellContainer">
+                                <Button bsStyle="primary">Votar</Button>
                             </Row>
                         </Col>
                     </Row>
