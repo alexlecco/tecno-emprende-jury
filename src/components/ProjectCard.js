5ec6947f6c8d6bb371ec4bcde3076accccfe3811
alex.villecco <alex.villecco@globant.com>
2018-10-17 10:01:17 -0300

    Refactor code

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,39 +4,39 @@ import '../App.css';
 import { Grid, Row, Col, Button, } from 'react-bootstrap';
 
 export default class ProjectCard extends Component {
-    constructor(props) {
-        super(props);
-    }
+	constructor(props) {
+		super(props);
+	}
 
-    render() {
-        return (
-            <div class="projectCard">
-                <Grid>
-                    <Row>
-                        <Col xs={8} md={6}>
-                            <Row className="cellContainer">
-                                {this.props.projectName}
-                            </Row>
-                            <Row className="cellContainer">
-                                {this.props.projectAuthor}
-                            </Row>
-                        </Col>
-                        <Col xs={5} md={3}>
-                            <Row className="cellContainer">
-                                Total de inversiones recibidas
-                            </Row>
-                            <Row className="cellContainer">
-                                $ {this.props.totalInvestment}
-                            </Row>
-                        </Col>
-                        <Col xs={5} md={3}>
-                            <Row className="cellContainer">
-                                <Button bsStyle="primary">Votar</Button>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Grid>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div class="projectCard">
+				<Grid>
+					<Row>
+						<Col xs={8} md={6}>
+							<Row className="cellContainer">
+								{this.props.projectName}
+							</Row>
+							<Row className="cellContainer">
+								{this.props.projectAuthor}
+							</Row>
+						</Col>
+						<Col xs={5} md={3}>
+							<Row className="cellContainer">
+								Total de inversiones recibidas
+							</Row>
+							<Row className="cellContainer">
+								$ {this.props.totalInvestment}
+							</Row>
+						</Col>
+						<Col xs={5} md={3}>
+							<Row className="cellContainer">
+								<Button bsStyle="primary">Votar</Button>
+							</Row>
+						</Col>
+					</Row>
+				</Grid>
+			</div>
+		)
+	}
 }
--- src/components/ProjectsContainer.js
+++ src/components/ProjectsContainer.js
@@ -7,58 +7,55 @@ import ProjectCard from './ProjectCard';
 import firebaseApp from '../firebase';
 
 export default class ProjectsContainer extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-			variable: 'holi',
-            projects: [],
-        }
-        this.projectsRef = firebaseApp.database().ref().child('projects');
+	constructor(props) {
+		super(props);
+		this.state = {
+			projects: [],
+		}
+		this.projectsRef = firebaseApp.database().ref().child('projects');
 	}
 	
 	componentDidMount() {
 		this.listenForProjects(this.projectsRef);
 	}
 
-    listenForProjects(projectsRef) {
-        projectsRef.on('value', snap => {
-            let projects = [];
-            snap.forEach((child) => {
-                projects.push({
-                author: child.val().author,
-                description: child.val().description,
-                id: child.val().id,
-                name: child.val().name,
-                _key: child.key
-                });
-            });
-            this.setState({
-                projects: projects
-            });
-        });
-    }
+	listenForProjects(projectsRef) {
+		projectsRef.on('value', snap => {
+			let projects = [];
+			snap.forEach((child) => {
+				projects.push({
+					author: child.val().author,
+					description: child.val().description,
+					id: child.val().id,
+					name: child.val().name,
+					_key: child.key
+				});
+			});
+			this.setState({
+				projects: projects
+			});
+		});
+	}
 
-    render() {
-		console.log("projects:::::::::::", this.state.projects);
-
-        return(
-            <div>
-                <div bsStyle="container">
-                    <Grid>
-                        <Row>
-                            {
-                                this.state.projects.map(
-                                    (project) => 
+	render() {
+		return(
+			<div>
+				<div bsStyle="container">
+					<Grid>
+						<Row>
+							{
+								this.state.projects.map(
+									(project) => 
 										<ProjectCard
 											projectName={project.name}
 											projectAuthor={project.author}
 											totalInvestment={10000} />
-                                )
-                            }
-                        </Row>
-                    </Grid>
-                </div>
-            </div>
-        );
-    }
+								)
+							}
+						</Row>
+					</Grid>
+				</div>
+			</div>
+		);
+	}
 }
