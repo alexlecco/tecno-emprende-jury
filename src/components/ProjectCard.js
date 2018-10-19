1f79b8a2b58f0cf1c29d57055a8d627e653ecee2
alex.villecco <alex.villecco@globant.com>
2018-10-19 10:41:52 -0300

    Add star rating component

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react';
 import '../App.css';
 
 import { Grid, Row, Col, Button, } from 'react-bootstrap';
+import StarRatingComponent from 'react-star-rating-component';
 
 export default class ProjectCard extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+      rating: 0
+    };
 	}
+
+	onStarClick(nextValue, prevValue, name) {
+    this.setState({rating: nextValue});
+  }
 
 	render() {
 		return (
@@ -33,7 +41,18 @@ export default class ProjectCard extends Component {
 						</Col>
 						<Col xs={5} md={3}>
 							<Row className="cellContainer">
-								<Button bsStyle="primary">Votar</Button>
+								Estrellas restantes: 5
+							</Row>
+							<Row className="cellContainer">
+									<Button bsStyle="primary">Votar</Button>
+							</Row>
+							<Row className="cellContainer">
+								<StarRatingComponent 
+									name="rate1" 
+									starCount={5}
+									value={this.state.rating}
+									onStarClick={this.onStarClick.bind(this)}
+								/>
 							</Row>
 						</Col>
 					</Row>
