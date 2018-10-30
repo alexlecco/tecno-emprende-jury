27f3b814c8e602d0f8a482d1c9bd2185bf352b32
alex.villecco <alex.villecco@globant.com>
2018-10-30 18:31:59 -0300

    Shows investors who invested in current project and inverted funds

--- src/components/WinnerInvestorCard.js
+++ src/components/WinnerInvestorCard.js
@@ -9,44 +9,50 @@ export default class WinnerInvestorContainer extends Component {
 		this.state = {}
 	}
 
+	calculateInvestments(investments) {
+		let total = 0;
+		
+		investments.forEach(
+			(investment) => {
+				if(investment.investor === this.props.investor.id) {
+					if(investment.project === this.props.project.id) {
+						total += investment.money_assigned;									
+					} 
+				}
+			}
+		)
+		return total;
+	}
+
 	render() {
 		const { investments } = this.props;
+		let investments_in_project = this.calculateInvestments(investments);
 
-		return(
-			<div>
-				{
-					investments.map(
-						(investment) => {
-							if(investment.investor === this.props.investor.id) {
-								if(investment.project === this.props.project.id) {
-									return(
-										<div class="projectCard">
-											<Grid>
-												<Row>
-													<Col xs={2}>
-														<Row className="cellContainer">
-															{this.props.investor.name}
-														</Row>
-													</Col> 
-													<Col xs={2}>
-														<Row className="cellContainer">
-															$ {this.props.investor.inverted_funds}
-														</Row>
-													</Col>
-												</Row>
-											</Grid>
-										</div>
-									);
-								} else {
-									return (<div />);								
-								}
-							} else {
-								return (<div />);
-							}
-						}
-					)
-				}
-			</div>
-		);
+		if(investments_in_project != 0) {
+			return(
+				<div>
+					<div class="projectCard">
+						<Grid>
+							<Row>
+								<Col xs={2}>
+									<Row className="cellContainer">
+										{this.props.investor.name}
+									</Row>
+								</Col> 
+								<Col xs={2}>
+									<Row className="cellContainer">
+										$ {investments_in_project}
+									</Row>
+								</Col>
+							</Row>
+						</Grid>
+					</div>
+				</div>
+			);
+		} else {
+			return(
+				<div />
+			)
+		}
 	}
 }
